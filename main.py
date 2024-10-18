290b0dad80eb7a6833e612e186f2e11137c61c38
Jelena Köhler <108803123+jelenakhlr@users.noreply.github.com>
2024-10-18 15:15:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt("data.csv")
+
+assert len(data) > 0, "no data"
+
 plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
 plt.scatter(data[:,0], data[:,1])
 plt.xlabel('x')
